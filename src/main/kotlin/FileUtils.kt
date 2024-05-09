e2ec5e1343251d217c13f085db058cf05378c210
Tony <hoanganhtran1998@gmail.com>
2024-05-09 09:16:50 -0400

    zip dir

--- src/main/kotlin/FileUtils.kt
+++ src/main/kotlin/FileUtils.kt
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.io.File
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 
 fun loadYamlObject(path: String): Map<String, Any> {
     val mapper = ObjectMapper(YAMLFactory())
@@ -32,4 +34,20 @@ fun getVersification(): Map<String, BookVersification> {
     val path = BookVersification::class.java.classLoader.getResource("verse_counts.json").file
 
     return mapper.readValue(File(path))
+}
+
+fun zipDirectory(sourceDir: File, zipFile: File) {
+    zipFile.createNewFile()
+    ZipOutputStream(zipFile.outputStream()).use { zos ->
+        sourceDir.walkTopDown().forEach { file ->
+            if (file.isFile) {
+                val entryPath = file.relativeTo(sourceDir).invariantSeparatorsPath
+                val zipEntry = ZipEntry(entryPath)
+                zos.putNextEntry(zipEntry)
+                file.inputStream().use { input ->
+                    input.copyTo(zos)
+                }
+            }
+        }
+    }
 }
